ac9813bd96a5fdaad7e1e1083fb76e750da845ad
Marco Taddei <marco.taddei@vidiemme.it>
2023-01-12 16:44:39 +0100

    ✨Apollo configuration

---
+++ HeyWorld/GraphQL/Generated/Operations/Queries/GetCountriesQuery.graphql.swift
@@ -0,0 +1,54 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public extension SchemaGraphQL {
+  class GetCountriesQuery: GraphQLQuery {
+    public static let operationName: String = "GetCountries"
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
+      definition: .init(
+        #"""
+        query GetCountries {
+          countries {
+            __typename
+            code
+            name
+          }
+        }
+        """#
+      ))
+
+    public init() {}
+
+    public struct Data: SchemaGraphQL.SelectionSet {
+      public let __data: DataDict
+      public init(data: DataDict) { __data = data }
+
+      public static var __parentType: ApolloAPI.ParentType { SchemaGraphQL.Objects.Query }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("countries", [Country].self),
+      ] }
+
+      public var countries: [Country] { __data["countries"] }
+
+      /// Country
+      ///
+      /// Parent Type: `Country`
+      public struct Country: SchemaGraphQL.SelectionSet {
+        public let __data: DataDict
+        public init(data: DataDict) { __data = data }
+
+        public static var __parentType: ApolloAPI.ParentType { SchemaGraphQL.Objects.Country }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("code", SchemaGraphQL.ID.self),
+          .field("name", String.self),
+        ] }
+
+        public var code: SchemaGraphQL.ID { __data["code"] }
+        public var name: String { __data["name"] }
+      }
+    }
+  }
+
+}
--- README.md
+++ README.md
@@ -7,5 +7,19 @@ A SwiftUI MVVM sample code. The goal of Hey World is to present informations for
 - iOS 15 SDK
 - iOS 15 deployment target
 
+## GraphQL
+
+### Fetch
+This is used to fetch the GraphQL schema
+```console
+./apollo-ios-cli fetch-schema --path ./HeyWorld/GraphQL/apollo-codegen-config.json
+```
+
+### Generate
+This is used to generate Swift source code
+```console
+./apollo-ios-cli generate --path ./HeyWorld/GraphQL/apollo-codegen-config.json
+```
+
 ## Third Party Software
 - [Apollo iOS 1.0.6](https://github.com/apollographql/apollo-ios)
